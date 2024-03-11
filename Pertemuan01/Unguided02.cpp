cae7e1637db17a86126d427c646b0ec3d6aa359d
AfrizalDwiNugraha136 <afrizalnugroho0304@gmail.com>
2024-03-11 18:09:59 +0700

    upate

---
+++ Pertemuan01/Unguided02.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Struct untuk merepresentasikan informasi penulis buku
+struct Author {
+    string name;
+    int birthYear;
+};
+
+// Class untuk merepresentasikan informasi buku
+class Book {
+public:
+    // Constructor untuk inisialisasi informasi buku
+    Book(string title, Author author, int yearPublished)
+        : title(title), bookAuthor(author), yearPublished(yearPublished) {}
+
+    // Fungsi untuk menampilkan informasi buku
+    void displayInfo() {
+        cout << "Buku: " << title << std::endl;
+        cout << "Penulis: " << bookAuthor.name << std::endl;
+        cout << "Tahun Terbit: " << yearPublished << std::endl;
+    }
+
+private:
+    // Informasi buku
+    string title;
+    Author bookAuthor;
+    int yearPublished;
+};
+
+int main() {
+    // Membuat objek Author
+    Author author{"John Doe", 1980};
+
+    // Membuat objek Book menggunakan struct Author sebagai bagian dari class
+    Book book1("Pemrograman C++", author, 2022);
+
+    // Menampilkan informasi buku
+    book1.displayInfo();
+
+    return 0;
+}
